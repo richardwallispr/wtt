4a45a61cf4369d317a2915506c9d21b43c0d3772
Richard Wallis <150674100+richardwallispr@users.noreply.github.com>
2025-11-18 10:30:37 +0000

    Correct album1 properties logging
    Fixed album1 properties to display correct values.

--- objects/script.js
+++ objects/script.js
@@ -1,5 +1,26 @@
 /*
 === INHERITANCE EXERCISE ===
+//
+// Part 1:
+//
+// The book1 object is created from the Book class.
+// When we log book1 to the console, it looks like this:
+//
+// Book {
+//   title: 'The Lord of The Things',
+//   releaseYear: 1940,
+//   price: 11.99,
+//   author: 'J.R.Rolkein',
+//   pages: 20000
+// }
+//
+// After running the setTitle method on book1, what should we see when we log book1 again?
+//
+// Part 2:
+//
+// There is a bug in the code related to the album1 object.
+// Fix the code so that the album1 properties show the correct values when logged to the console.
+//
 */
 
 class Media {
@@ -35,6 +56,9 @@ class MusicAlbum extends Media {
 }
 
 const book1 = new Book("The Lord of The Things", 1940, 11.99, "J.R.Rolkein", 20000);
+console.log(book1);
 book1.setTitle('The Halfling')
 
 const album1 = new Media("The Bright Side of the Sun", 2068, 9.99, "Pink Flamingo");
+console.log(album1.title);
+console.log(album1.artist);
